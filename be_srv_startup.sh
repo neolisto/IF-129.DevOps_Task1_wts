c1d22b96d4a980d53638073087c4a62ab2cc540e
neolisto <roman.khlevniuk@gmail.com>
2021-09-11 13:42:08 +0200

    Creating Back-End server startup script

---
+++ be_srv_startup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# system updating
+sudo apt update
+sudo apt upgrade -y
+
+# installing OpenJDK v.8
+sudo apt install openjdk-8-jdk
+
+# installing maven
+sudo apt install maven
+
+# downloading eSchool project
+git clone https://github.com/yurkovskiy/eSchool.git
+
+# work-directory changing
+cd eSchool/
+
+# commenting non-usable test
+sudo echo '/**' > src/test/java/academy/softserve/eschool/controller/ScheduleControllerIntegrationTest.java
+sudo echo '**/' >> src/test/java/academy/softserve/eschool/controller/ScheduleControllerIntegrationTest.java
+
+# changing default DB-server IP to real
+sudo sed -i 's/localhost/\${DB_SRV_IP}/g' src/main/resources/application.properties
+
+# changing default DB-server user to real
+sudo sed -i 's/\${DATASOURCE_USERNAME:root}/\${DATASOURCE_USERNAME}/g' src/main/resources/application.properties
+
+#changing default DB-server user's password to real
+sudo sed -i 's/\${DATASOURCE_PASSWORD:root}/\${DATASOURCE_PASSWORD}/g' src/main/resources/application.properties
+
+#  maven build start
+sudo mvn clean
+
+# maven packg build start
+sudo mvn package
+
+# java-app start
+cd target/
+sudo java -jar eschool.jar
