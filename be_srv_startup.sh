dd75c69eda1f36af543f0690029193170763a403
neolisto <roman.khlevniuk@gmail.com>
2021-09-11 15:20:18 +0200

    sed command fixed

--- be_srv_startup.sh
+++ be_srv_startup.sh
@@ -5,10 +5,10 @@ sudo apt update
 sudo apt upgrade -y
 
 # installing OpenJDK v.8
-sudo apt install openjdk-8-jdk
+sudo apt install openjdk-8-jdk -y
 
 # installing maven
-sudo apt install maven
+sudo apt install maven -y
 
 # downloading eSchool project
 git clone https://github.com/yurkovskiy/eSchool.git
@@ -21,13 +21,13 @@ sudo echo '/**' > src/test/java/academy/softserve/eschool/controller/ScheduleCon
 sudo echo '**/' >> src/test/java/academy/softserve/eschool/controller/ScheduleControllerIntegrationTest.java
 
 # changing default DB-server IP to real
-sudo sed -i 's/localhost/\${DB_SRV_IP}/g' src/main/resources/application.properties
+sudo sed -i 's/localhost/'$DB_SRV_IP'/g' src/main/resources/application.properties
 
 # changing default DB-server user to real
-sudo sed -i 's/\${DATASOURCE_USERNAME:root}/\${DATASOURCE_USERNAME}/g' src/main/resources/application.properties
+sudo sed -i 's/\${DATASOURCE_USERNAME:root}/\'$DATASOURCE_USERNAME'/g' src/main/resources/application.properties
 
 #changing default DB-server user's password to real
-sudo sed -i 's/\${DATASOURCE_PASSWORD:root}/\${DATASOURCE_PASSWORD}/g' src/main/resources/application.properties
+sudo sed -i 's/\${DATASOURCE_PASSWORD:root}/\'$DATASOURCE_PASSWORD'/g' src/main/resources/application.properties
 
 #  maven build start
 sudo mvn clean
