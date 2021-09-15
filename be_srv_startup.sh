27e4c2aa6dcdd9e0c358f9714440d9670afc0323
neolisto <roman.khlevniuk@gmail.com>
2021-09-15 16:13:50 +0200

    reconfigured for terraform

--- be_srv_startup.sh
+++ be_srv_startup.sh
@@ -4,6 +4,9 @@
 sudo apt update
 sudo apt upgrade -y
 
+export DATASOURCE_USERNAME=${DATASOURCE_USERNAME}
+export DATASOURCE_PASSWORD=${DATASOURCE_PASSWORD}
+
 # installing OpenJDK v.8
 sudo apt install openjdk-8-jdk -y
 
@@ -17,17 +20,15 @@ git clone https://github.com/yurkovskiy/eSchool.git
 cd eSchool/
 
 # commenting non-usable test
-sudo echo '/**' > src/test/java/academy/softserve/eschool/controller/ScheduleControllerIntegrationTest.java
-sudo echo '**/' >> src/test/java/academy/softserve/eschool/controller/ScheduleControllerIntegrationTest.java
+sudo echo '/**' >src/test/java/academy/softserve/eschool/controller/ScheduleControllerIntegrationTest.java
+sudo echo '**/' >>src/test/java/academy/softserve/eschool/controller/ScheduleControllerIntegrationTest.java
 
 # changing default DB-server IP to real
-sudo sed -i 's/localhost/'$DB_SRV_IP'/g' src/main/resources/application.properties
+sudo sed -i 's/localhost/'${DB_SRV_IP}'/g' src/main/resources/application.properties
 
 # changing default DB-server user to real
-sudo sed -i 's/\${DATASOURCE_USERNAME:root}/\'$DATASOURCE_USERNAME'/g' src/main/resources/application.properties
-
-#changing default DB-server user's password to real
-sudo sed -i 's/\${DATASOURCE_PASSWORD:root}/\'$DATASOURCE_PASSWORD'/g' src/main/resources/application.properties
+sudo sed -i '3s/.*/spring.datasource.username=\'${DATASOURCE_USERNAME}'/' src/main/resources/application.properties
+sudo sed -i '4s/.*/spring.datasource.password=\'${DATASOURCE_PASSWORD}'/' src/main/resources/application.properties
 
 #  maven build start
 sudo mvn clean
@@ -37,4 +38,4 @@ sudo mvn package
 
 # java-app start
 cd target/
-nohup java -jar eschool.jar &
+sudo nohup java -jar eschool.jar &
