64f4860500c57babaf3291cc91be1d4523e221e0
neolisto <roman.khlevniuk@gmail.com>
2021-09-19 00:09:07 +0200

    .sh-script of load-balancing server installation configured

--- lb_srv_startup.sh
+++ lb_srv_startup.sh
@@ -12,10 +12,24 @@ sudo systemctl enable nginx
 # editing nginx-server config files
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo rm /etc/nginx/conf.d/load-balancing.conf
-sudo cp load-balancing.conf /etc/nginx/conf.d/load-balancing.conf
-sudo rm load-balancing.conf
+echo "upstream backend {" > /etc/nginx/conf.d/load-balancing.conf
+echo "ip_hash;" >> /etc/nginx/conf.d/load-balancing.conf
+echo "server BE1_SRV_IP:8080;" >> /etc/nginx/conf.d/load-balancing.conf
+echo "server BE2_SRV_IP:8080;" >> /etc/nginx/conf.d/load-balancing.conf
+echo "}" >> /etc/nginx/conf.d/load-balancing.conf
+echo "server {" >> /etc/nginx/conf.d/load-balancing.conf
+echo "listen 80;" >> /etc/nginx/conf.d/load-balancing.conf
+echo "server_name loadbalancing.example.com;" >> /etc/nginx/conf.d/load-balancing.conf
+echo "location / {" >> /etc/nginx/conf.d/load-balancing.conf
+echo "proxy_redirect off;" >> /etc/nginx/conf.d/load-balancing.conf
+echo "proxy_set_header X-Real-IP \$remote_addr;" >> /etc/nginx/conf.d/load-balancing.conf
+echo "proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;" >> /etc/nginx/conf.d/load-balancing.conf
+echo "proxy_set_header    Host \$http_host;" >> /etc/nginx/conf.d/load-balancing.conf
+echo "proxy_pass http://backend;" >> /etc/nginx/conf.d/load-balancing.conf
+echo "}" >> /etc/nginx/conf.d/load-balancing.conf
+echo "}" >> /etc/nginx/conf.d/load-balancing.conf
 
-# adding real IP-adresses of BE-servers
+# adding real IP-addresses of BE-servers
 sudo sed -i 's/BE1_SRV_IP/'$BE1_SRV_IP'/g' /etc/nginx/conf.d/load-balancing.conf
 sudo sed -i 's/BE2_SRV_IP/'$BE2_SRV_IP'/g' /etc/nginx/conf.d/load-balancing.conf
 
