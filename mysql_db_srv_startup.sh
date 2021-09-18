55fbdebf69e57a65ba3a9be335ad28c54229431e
neolisto <roman.khlevniuk@gmail.com>
2021-09-19 00:09:35 +0200

    .sh-script of MySQL database server installation configured

--- mysql_db_srv_startup.sh
+++ mysql_db_srv_startup.sh
@@ -36,12 +36,12 @@ sudo mysql < db_creating.tmp
 
 #creating user
 echo 'creating USER'
-echo 'CREATE USER '\'${DATASOURCE_USERNAME}\''@'\''%'\'' IDENTIFIED BY '\'${DATASOURCE_PASSWORD}\'';' > user_creating.tmp
+echo 'CREATE USER '\'$DATASOURCE_USERNAME\''@'\''%'\'' IDENTIFIED BY '\'$DATASOURCE_PASSWORD\'';' > user_creating.tmp
 sudo mysql < user_creating.tmp
 
 #granting privileges to user
 echo 'granting PRIVILEGES'
-echo 'GRANT ALL PRIVILEGES ON *.* TO '\'${DATASOURCE_USERNAME}\''@'\''%'\'';' > db_prev.tmp
+echo 'GRANT ALL PRIVILEGES ON *.* TO '\'$DATASOURCE_USERNAME\''@'\''%'\'';' > db_prev.tmp
 sudo mysql < db_prev.tmp
 
 #removing temporary files
@@ -57,6 +57,37 @@ sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo echo 'done'
 
 #restarting mysql service with new parameters
+sudo systemctl enable mysql
 sudo echo 'restart mysql'
 sudo systemctl restart mysql
 sudo echo 'done'
+
+# CI/CD agent installation
+sudo apt install openjdk-8-jdk -y
+sudo apt install unzip
+echo JAVA_HOME=\"/usr/lib/jvm/java-8-openjdk-amd64\" >> /etc/environment
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+
+# waiting before CI/CD server start
+sleep 10m
+
+# downloading and configuring CI/CD agent
+sudo mkdir /TC_ci-cd
+cd /TC_ci-cd
+sudo wget http://$CI_CD_IP:8111/update/buildAgent.zip
+sudo unzip buildAgent.zip -d buildAgent
+cd buildAgent
+sudo cp conf/buildAgent.dist.properties conf/buildAgent.properties
+sudo rm conf/buildAgent.dist.properties
+sudo sed -i 's/localhost/'$CI_CD_IP'/g' conf/buildAgent.properties
+sudo sed -i 's/name=/name='$AGENT_NAME'/g' conf/buildAgent.properties
+
+# starting agent
+sudo bin/./agent.sh start
+
+# adding agent service to auto-reload services
+sudo echo "#!"/bin"/bash""" > tmp123
+sudo echo "sudo /TC_ci-cd/buildAgent/bin/./agent.sh start" >> tmp123
+sudo cat tmp123 >> /etc/rc.local
+sudo rm -r tmp123
+sudo chmod +x /etc/rc.local
