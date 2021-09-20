72126e1ba2d159418a9cd5238394f0d7ac37b53d
neolisto <roman.khlevniuk@gmail.com>
2021-09-20 17:30:03 +0200

    test push

--- be_srv_startup.sh
+++ be_srv_startup.sh
@@ -2,74 +2,75 @@
 
 # system updating
 sudo apt update
-sudo apt upgrade -y
+#sudo apt upgrade -y
 
-export DATASOURCE_USERNAME=some_value
-export DATASOURCE_PASSWORD=some_value
+#export DATASOURCE_USERNAME=some_value
+#export DATASOURCE_PASSWORD=some_value
 
 # test string
 
 # installing OpenJDK v.8
-sudo apt install openjdk-8-jdk -y
+#sudo apt install openjdk-8-jdk -y
 
 # installing maven
-sudo apt install maven -y
+#sudo apt install maven -y
 
 # downloading eSchool project
-mkdir /application
+#mkdir /application
 cd /application
-git clone https://github.com/yurkovskiy/eSchool.git
+#git clone https://github.com/yurkovskiy/eSchool.git
 
 # work-directory changing
 cd eSchool/
+mkdir 123_123
 
 # commenting non-usable test
-sudo echo '/**' >src/test/java/academy/softserve/eschool/controller/ScheduleControllerIntegrationTest.java
-sudo echo '**/' >>src/test/java/academy/softserve/eschool/controller/ScheduleControllerIntegrationTest.java
+#sudo echo '/**' >src/test/java/academy/softserve/eschool/controller/ScheduleControllerIntegrationTest.java
+#sudo echo '**/' >>src/test/java/academy/softserve/eschool/controller/ScheduleControllerIntegrationTest.java
 
 # changing default DB-server IP to real
-sudo sed -i 's/localhost/'$DB_SRV_IP'/g' src/main/resources/application.properties
+#sudo sed -i 's/localhost/'$DB_SRV_IP'/g' src/main/resources/application.properties
 
 # changing default DB-server user to real
-sudo sed -i '3s/.*/spring.datasource.username=\'$DATASOURCE_USERNAME'/' src/main/resources/application.properties
-sudo sed -i '4s/.*/spring.datasource.password=\'$DATASOURCE_PASSWORD'/' src/main/resources/application.properties
+#sudo sed -i '3s/.*/spring.datasource.username=\'$DATASOURCE_USERNAME'/' src/main/resources/application.properties
+#sudo sed -i '4s/.*/spring.datasource.password=\'$DATASOURCE_PASSWORD'/' src/main/resources/application.properties
 
 #  maven build start
-sudo mvn clean
+#sudo mvn clean
 
 # maven packg build start
-sudo mvn package
+#sudo mvn package
 
 # auto loading after reboot
-sudo echo "#!"/bin"/bash""" > tmp123
-sudo echo "sudo nohup java -jar /application/eSchool/target/eschool.jar &" >> tmp123
-sudo echo "sudo /TC_ci-cd/buildAgent/bin/./agent.sh start" >> tmp123
-sudo cat tmp123 > /etc/rc.local
-sudo rm -r tmp123
-sudo chmod +x /etc/rc.local
+#sudo echo "#!"/bin"/bash""" > tmp123
+#sudo echo "sudo nohup java -jar /application/eSchool/target/eschool.jar &" >> tmp123
+#sudo echo "sudo /TC_ci-cd/buildAgent/bin/./agent.sh start" >> tmp123
+#sudo cat tmp123 > /etc/rc.local
+#sudo rm -r tmp123
+#sudo chmod +x /etc/rc.local
 
 # java-app start
-cd target/
-sudo nohup java -jar eschool.jar &
+#cd target/
+#sudo nohup java -jar eschool.jar &
 
 # CI/CD agent installation
-sudo apt install unzip
-echo JAVA_HOME=\"/usr/lib/jvm/java-8-openjdk-amd64\" >> /etc/environment
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+#sudo apt install unzip
+#echo JAVA_HOME=\"/usr/lib/jvm/java-8-openjdk-amd64\" >> /etc/environment
+#export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
 # waiting before CI/CD server start
-sleep 5m
+#sleep 5m
 
 # downloading and configuring CI/CD agent
-sudo mkdir /TC_ci-cd
-cd /TC_ci-cd
-sudo wget http://$CI_CD_IP:8111/update/buildAgent.zip
-sudo unzip buildAgent.zip -d buildAgent
-cd buildAgent
-sudo cp conf/buildAgent.dist.properties conf/buildAgent.properties
-sudo rm conf/buildAgent.dist.properties
-sudo sed -i 's/localhost/'$CI_CD_IP'/g' conf/buildAgent.properties
-sudo sed -i 's/name=/name='$AGENT_NAME'/g' conf/buildAgent.properties
+#sudo mkdir /TC_ci-cd
+#cd /TC_ci-cd
+#sudo wget http://$CI_CD_IP:8111/update/buildAgent.zip
+#sudo unzip buildAgent.zip -d buildAgent
+#cd buildAgent
+#sudo cp conf/buildAgent.dist.properties conf/buildAgent.properties
+#sudo rm conf/buildAgent.dist.properties
+#sudo sed -i 's/localhost/'$CI_CD_IP'/g' conf/buildAgent.properties
+#sudo sed -i 's/name=/name='$AGENT_NAME'/g' conf/buildAgent.properties
 
 # starting agent
-sudo bin/./agent.sh start
+#sudo bin/./agent.sh start
